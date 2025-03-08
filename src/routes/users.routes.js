5db1f92e613e08eb58c94255bcf9197a33fb6e22
Vigneshreddy <vigneshreddy625@gmail.com>
2025-03-08 22:18:25 +0530

    added

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,7 +7,6 @@ import {
   refreshAccessToken,
 } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
