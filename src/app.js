c5f1f4feb6170c73007cc45100c7c395ae5156d7
Vigneshreddy <vigneshreddy625@gmail.com>
2025-03-08 23:10:45 +0530

    add

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(
       resave: false,
       saveUninitialized: false,
       store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI, 
+        mongoUrl: process.env.MONGODB_URI, 
         collectionName: "sessions",
       }),
       cookie: {
