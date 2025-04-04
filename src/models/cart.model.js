0e2655632998c925612f11ac333c1e34949ab697
Vigneshreddy <vigneshreddy625@gmail.com>
2025-04-05 00:52:30 +0530

    added

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -37,5 +37,4 @@ const cartSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Cart = mongoose.model("Cart", cartSchema);
-export default Cart;
+export const Cart = mongoose.model("Cart", cartSchema)
