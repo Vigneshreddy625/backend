acae3f7a01cfc97716bd8749cd3d85848e4070d3
Vigneshreddy <vigneshreddy625@gmail.com>
2025-04-05 14:29:09 +0530

    added

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:5173",             
-  process.env.FRONTEND_URL, 
+  "https://revispy-nine.vercel.app", 
 ];
 
 app.use(cors({
