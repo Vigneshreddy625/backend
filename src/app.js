3f2da1858b064d2abc8e54539546f25526a2b8d0
Vigneshreddy <vigneshreddy625@gmail.com>
2025-04-05 15:16:26 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -8,20 +8,10 @@ dotenv.config('./.env')
 
 const app = express()
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://revispy-nine.vercel.app/'
-];
-
-app.use(req, res, next => {
-  const origin = req.headers.origin;
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins.includes(origin) ? origin : 'null');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-}
-)
-
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use(express.json({limit: '16kb'}))
 app.use(express.urlencoded({extended: true}))
